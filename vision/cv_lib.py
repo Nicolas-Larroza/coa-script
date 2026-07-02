d0d3bad33d986a2541bd25262f38c7d807dd7601
Nicolas-Larroza <nicolaslarroza1@gmail.com>
2026-07-01 21:11:06 -0300

    tbh i don't even know what i did mate. I'm tired, I tried, did very little but I gave my best. I'll try again  tomorrow.

--- vision/cv_lib.py
+++ vision/cv_lib.py
@@ -1,13 +1,13 @@
 import cv2 as cv, numpy as np
 import os
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 def load_screenshot(image_path):
     image = cv.imread(image_path)  # Reads the image from disk
     return image
 
 def find_minable_ore():
     screenshot_path = os.path.join(BASE_DIR, "game_screenshot.png")
-    template_path = os.path.join(BASE_DIR + "/templates", "obsidian_ore.png")
+    template_path = os.path.join(BASE_DIR + "/assets/templates", "obsidian_ore.png")
     screenshot = load_screenshot(screenshot_path)
     template = cv.imread(template_path)
     result = cv.matchTemplate(screenshot, template, cv.TM_CCOEFF_NORMED)
@@ -24,8 +24,8 @@ def find_minable_ore():
     
 
 def search_for_x(thing):
-    screenshot_path = os.path.join(BASE_DIR, "game_screenshot.png")
-    template_path = os.path.join(BASE_DIR+ "/templates", f"{thing}.png")
+    screenshot_path = os.path.join(BASE_DIR + "/assets/screenshots/game_screenshot.png")
+    template_path = os.path.join(BASE_DIR + "/assets/templates" + f"{thing}.png")
     screenshot = load_screenshot(screenshot_path)
     template = load_screenshot(template_path)
     
@@ -59,9 +59,9 @@ def search_for_x(thing):
     resized_screenshot = cv.resize(screenshot, (int(width * scaling_factor), int(height * scaling_factor)))
     
     # Show the image with rectangles drawn around the matches
-    #cv.imshow("Matches", resized_screenshot)
-    #cv.waitKey(0)
-    #cv.destroyAllWindows()
+    cv.imshow("Matches", resized_screenshot)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
     return match_count
 
 
